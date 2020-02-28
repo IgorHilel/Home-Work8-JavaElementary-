80dc45e7be8e6738d8322e02b42e91c76b07df81
Igor Berezovchuck <tiraeligor@icloud.com>
2020-02-28 15:27:38 +0200

    add task2

--- src/com/igorberezovchuck/Task2.java
+++ src/com/igorberezovchuck/Task2.java
@@ -1,7 +1,42 @@
 package com.igorberezovchuck;
 
-public class Task2 {
-    public static void main(String[] args) {
+import com.sun.source.tree.Tree;
 
+/*
+2*) Написать метод для обьединения 2 бинарных дерева:
+В качестве параметра передается 2 ссылки на корневые узлы девева TreeNode, результат - корневой узел
+нового дерева TreeNode.
+ */
+public class Task2 {
+
+    public static void main(String[] args) {
+        TreeNode root1 = new TreeNode(1);
+        root1.left = new TreeNode(3);
+        root1.right = new TreeNode(2);
+        root1.left.left = new TreeNode(5);
+
+        TreeNode root2 = new TreeNode(2);
+        root2.left = new TreeNode(1);
+        root2.right = new TreeNode(3);
+        root2.left.right = new TreeNode(4);
+        root2.right.right = new TreeNode(7);
+
+        TreeNode result2 = mergeTrees(root1, root2);
+        System.out.println(result2.val);
+        System.out.println(result2.right.val);
+        System.out.println(result2.left.val);
+    }
+
+    public static TreeNode mergeTrees(TreeNode t1, TreeNode t2) {
+        if (t1 == null) {
+            return t2;
+        }
+        if (t2 == null) {
+            return t1;
+        }
+        t1.val += t2.val;
+        t1.left = mergeTrees(t1.left, t2.left);
+        t1.right = mergeTrees(t1.right, t2.right);
+        return t1;
     }
 }
---
+++ src/com/igorberezovchuck/TreeNode.java
@@ -0,0 +1,13 @@
+package com.igorberezovchuck;
+
+public class TreeNode {
+
+    int val;
+
+    TreeNode left;
+
+    TreeNode right;
+
+    TreeNode(int x) { val = x; }
+
+}
